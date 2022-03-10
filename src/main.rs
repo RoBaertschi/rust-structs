53cb1bbeab9eaf6b1bdbf110dcefccca71a976c5
Robin <rtmbaertschi007@gmail.com>
2022-03-10 13:38:02 +0100

    hi

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64
-}
-
-fn build_user(email: String, username: String) -> User {
-    User { active: true, username, email, sign_in_count: 1 }
-}
 fn main() {
-    let mut user1 = build_user(String::from("hi"), String::from("hi"));
-
-    user1.email = String::from("anotheremail@example.com");
-
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
-
-    println!("Hi {}", user1.email);
+    
 }
